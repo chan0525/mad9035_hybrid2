b6940d4cd4a242c889fbda8945eb5d016eab2ec7
Bruno Chan <chan0525@algonquinlive.com>
2023-09-16 16:44:00 -0400

    created member field, convert string to int, and fixed nullable values

--- bin/my_cli_app.dart
+++ bin/my_cli_app.dart
@@ -1,5 +1,15 @@
+import 'dart:io';
 import 'package:my_cli_app/my_cli_app.dart' as my_cli_app;
 
 void main(List<String> arguments) {
-  print('Hello world: ${my_cli_app.calculate()}!');
+  for (final name in arguments) {
+    final lucky = my_cli_app.Lucky();
+    final nameCap = capFirstLetter(name);
+    print('Hello $nameCap. Your lucky number is ${lucky.randomNumber}.');
+  }
 }
+
+String capFirstLetter(String name) {
+  return name[0].toUpperCase() + name.substring(1);
+}
+
--- lib/my_cli_app.dart
+++ lib/my_cli_app.dart
@@ -1,17 +1,21 @@
 import 'dart:io';
 import 'dart:math';
-import 'package:args/args.dart';
 
 class Lucky {
+  int? randomNumber;
 
+  Lucky() {
+
+    final minVal = Platform.environment['MIN'];
+    final maxVal = Platform.environment['MAX'];
+
+    final min = minVal != null ? int.parse(minVal) : 1;
+    final max = maxVal != null ? int.parse(maxVal) : 100;
+
+    final _random = Random();
+    randomNumber = min + _random.nextInt(max - min);
+
+  }
 
 
 }
-
-
-
-/*
-int calculate() {
-  return 6 * 7;
-}
-*/
