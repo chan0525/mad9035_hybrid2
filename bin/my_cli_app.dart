8bc944df79ac49d4190e012123bec0dbee914ed6
Bruno Chan <chan0525@algonquinlive.com>
2023-09-16 14:54:34 -0400

    First commit

---
+++ bin/my_cli_app.dart
@@ -0,0 +1,5 @@
+import 'package:my_cli_app/my_cli_app.dart' as my_cli_app;
+
+void main(List<String> arguments) {
+  print('Hello world: ${my_cli_app.calculate()}!');
+}
---
+++ lib/my_cli_app.dart
@@ -0,0 +1,17 @@
+import 'dart:io';
+import 'dart:math';
+import 'package:args/args.dart';
+
+class Lucky {
+
+
+
+}
+
+
+
+/*
+int calculate() {
+  return 6 * 7;
+}
+*/
